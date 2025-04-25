804253527245e8f1d00cac100db2af19afbce575
Pauline Eustachy <pauline.eustachy@filigran.io>
2025-04-25 12:21:14 +0200

    fix: update template without collector/converter

--- shared/templates/external_import/src/connector.py
+++ shared/templates/external_import/src/connector.py
@@ -1,5 +1,10 @@
+from functools import cached_property
+
 from connectors_sdk.external_import import BaseConnector
 from connectors_sdk.octi_entities.common import BaseEntity as OCTIBaseEntity
+from client import FakeClient
+from converter import OCTIConverter
+from config import TemplateConfig
 
 
 class TemplateConnector(BaseConnector):
@@ -11,30 +16,58 @@ class TemplateConnector(BaseConnector):
     The following variables are accessible in the class:
     - helper: instance of OpenCTIConnectorHelper
     - config: instance of BaseConfig subclass
-    - collector: instance of BaseCollector subclass
-    - converter: instance of BaseConverter subclass
+
+    The following methods MUST be implemented:
+    - process_data: should return OCTI entities (from `connectors_sdk.octi_entities`)
+
+    Any additional implementation is optional and depending on each use case.
     """
+
+    config: TemplateConfig  # overwrite `BaseConfig` type hint (for typechecking only)
+
+    # =============================
+    # === Change the code below ===
+    # =============================
+
+    @cached_property
+    def client(self) -> FakeClient:
+        """
+        Instantiate a client for the external source(s).
+        """
+        return FakeClient()
+
+    @cached_property
+    def converter(self) -> OCTIConverter:
+        """
+        Instantiate a converter to process data from external source(s).
+        """
+        return OCTIConverter(config=self.config)
+
+    # =============================
+    # === Change the code above ===
+    # =============================
 
     def process_data(self) -> list[OCTIBaseEntity]:
         """
-        Collect and convert data from external source(s) into STIX objects.
-        This method MUST be implemented by each connector and return a list of STIX objects.
+        Collect and convert data from external source(s) into OCTI entities.
+        This method MUST be implemented by each connector and return OCTI entities.
         """
 
         octi_objects: list[OCTIBaseEntity] = []
 
-        # ===========================
-        # === Add your code below ===
-        # ===========================
+        # =============================
+        # === Change the code below ===
+        # =============================
 
-        # For example:
-        reports = self.collector.collect()
+        reports = self.client.fetch_reports(
+            since=self.config.template.import_start_date
+        )
         for report in reports:
-            extracted_octi_objects = self.converter.convert(report)
+            extracted_octi_objects = self.converter.convert_report(report)
             octi_objects.extend(extracted_octi_objects)
 
-        # ===========================
-        # === Add your code above ===
-        # ===========================
+        # =============================
+        # === Change the code above ===
+        # =============================
 
         return octi_objects
